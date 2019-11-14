06a84a56baf069317709f94574e728e36d63f5c4
unknown <siu1@hi.is>
2019-11-14 23:09:34 +0000

    eslint

--- scripts.js
+++ scripts.js
@@ -6,25 +6,75 @@ const API_URL = 'https://apis.is/company?name=';
 const program = (() => {
   let companies;
 
-  function init(_companies) {
-    companies = _companies;
-    const form = companies.querySelector("form");
-    form.addEventListener("submit", search);
+  function errorMessage(error) {
+    const node = document.querySelector('.results');
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+    alert(error);
   }
 
-  function loading()
-  {
-    var node = document.querySelector(".results");
+  function addData(data) {
+    if (data.length === 0) {
+      errorMessage('Ekkert fyrirtæki fannst fyrir leitarstreng');
+      return;
+    }
+
+    const node = document.querySelector('.results');
     while (node.firstChild) {
       node.removeChild(node.firstChild);
     }
 
-    const loadDiv = document.createElement("div");
-    loadDiv.classList.add("loading");
-    const loadGif = document.createElement("img");
-    loadGif.src = "loading.gif";
-    const loadMsg = document.createElement("p");
-    loadMsg.textContent = "Leita að fyrirtækjum...";
+    const resultNode = document.createElement('div');
+    resultNode.classList.add('company');
+
+    data.forEach((obj) => {
+      const resultDiv = document.createElement('div');
+      resultDiv.classList.add('company');
+      const dList = document.createElement('dl');
+      const dtName = document.createElement('dt');
+      dtName.textContent = 'Nafn:';
+      const ddName = document.createElement('dd');
+      ddName.textContent = obj.name;
+      const dtSnumber = document.createElement('dt');
+      dtSnumber.textContent = 'Kennitala:';
+      const ddSnumber = document.createElement('dd');
+      ddSnumber.textContent = obj.sn;
+
+      dList.appendChild(dtName);
+      dList.appendChild(ddName);
+      dList.appendChild(dtSnumber);
+      dList.appendChild(ddSnumber);
+
+      if (obj.active === 1) {
+        resultDiv.classList.add('company--active');
+        const dtAddress = document.createElement('dt');
+        dtAddress.textContent = 'Heimilisfang:';
+        const ddAddress = document.createElement('dd');
+        ddAddress.textContent = obj.address;
+        dList.appendChild(dtAddress);
+        dList.appendChild(ddAddress);
+      } else {
+        resultDiv.classList.add('company--inactive');
+      }
+
+      resultDiv.appendChild(dList);
+      node.appendChild(resultDiv);
+    });
+  }
+
+  function loading() {
+    const node = document.querySelector('.results');
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+
+    const loadDiv = document.createElement('div');
+    loadDiv.classList.add('loading');
+    const loadGif = document.createElement('img');
+    loadGif.src = 'loading.gif';
+    const loadMsg = document.createElement('p');
+    loadMsg.textContent = 'Leita að fyrirtækjum...';
 
     loadDiv.appendChild(loadGif);
     loadDiv.appendChild(loadMsg);
@@ -32,23 +82,7 @@ const program = (() => {
     node.appendChild(loadDiv);
   }
 
-  function search(e)
-  {
-    e.preventDefault();
-    loading();
-    const companyName = e.target.querySelector('input').value;
-    if(companyName.length > 0  && /\S/.test(companyName))
-    {
-      fetchData(companyName);
-    }
-    else
-    {
-      errorMessage("Lén verður að vera strengur");
-    }
-  }
-
-  function fetchData(companyName)
-  {
+  function fetchData(companyName) {
     const url = API_URL + companyName;
     fetch(url)
       .then((result) => {
@@ -57,71 +91,25 @@ const program = (() => {
         }
         throw new Error();
       })
-      .then((data) => {addData(data.results);})
-      .catch(() => {errorMessage("Villa við að sækja gögn.");});
+      .then((data) => { addData(data.results); })
+      .catch(() => { errorMessage('Villa við að sækja gögn.'); });
   }
 
-  function addData(data)
-  {
-    if (data.length === 0)
-    {
-      errorMessage("Ekkert fyrirtæki fannst fyrir leitarstreng");
-      return;
+  function search(e) {
+    e.preventDefault();
+    loading();
+    const companyName = e.target.querySelector('input').value;
+    if (companyName.length > 0 && /\S/.test(companyName)) {
+      fetchData(companyName);
+    } else {
+      errorMessage('Lén verður að vera strengur');
     }
-
-    var node = document.querySelector(".results");
-    while (node.firstChild) {
-      node.removeChild(node.firstChild);
-    }
-
-    const resultNode = document.createElement("div");
-    resultNode.classList.add("company");
-
-    data.forEach((obj) => {
-      const resultDiv = document.createElement("div");
-      resultDiv.classList.add("company");
-      const dList = document.createElement("dl");
-      const dtName = document.createElement("dt");
-      dtName.textContent = "Nafn:";
-      const ddName = document.createElement("dd");
-      ddName.textContent = obj.name;
-      const dtSnumber = document.createElement("dt");
-      dtSnumber.textContent = "Kennitala:";
-      const ddSnumber = document.createElement("dd");
-      ddSnumber.textContent = obj.sn;
-
-      dList.appendChild(dtName);
-      dList.appendChild(ddName);
-      dList.appendChild(dtSnumber);
-      dList.appendChild(ddSnumber);
-
-      if(obj.active === 1)
-      {
-        resultDiv.classList.add("company--active");
-        const dtAddress = document.createElement("dt");
-        dtAddress.textContent = "Heimilisfang:";
-        const ddAddress = document.createElement("dd");
-        ddAddress.textContent = obj.address;
-        dList.appendChild(dtAddress);
-        dList.appendChild(ddAddress);
-      }
-      else
-      {
-        resultDiv.classList.add("company--inactive");
-      }
-
-      resultDiv.appendChild(dList);
-      node.appendChild(resultDiv);
-    });
   }
 
-  function errorMessage(error)
-  {
-    var node = document.querySelector(".results");
-    while (node.firstChild) {
-      node.removeChild(node.firstChild);
-    }
-    alert(error);
+  function init(_companies) {
+    companies = _companies;
+    const form = companies.querySelector('form');
+    form.addEventListener('submit', search);
   }
 
   return {
